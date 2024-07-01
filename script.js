4539343c410853f3c590b4da300666ccd00e31a8
Aathish R Viswam <aathishrv@gmail.com>
2024-07-01 15:59:53 +0530

    Update internal calculations in js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 "use strict";
-
-const cat1 = document.querySelector(".cat1");
-const cat2 = document.querySelector(".cat2");
-const attendence = document.querySelector(".attendence");
-const assignment1 = document.querySelector(".assignment1");
-const assignment2 = document.querySelector(".assignment2");
+let Internals;
+let attendenceMarks;
 const submit = document.querySelector(".submit-btn");
 
 submit.addEventListener("click", function () {
-  console.log("Button clicked");
+  const cat1 = parseFloat(document.getElementById("Cat1Marks").value);
+  const cat2 = parseFloat(document.getElementById("Cat2Marks").value);
+  const attendence = parseFloat(document.getElementById("Attendence").value);
+  const assignment1 = parseFloat(document.getElementById("Assignment1").value);
+  const assignment2 = parseFloat(document.getElementById("Assignment2").value);
+
+  if (attendence >= 75 && attendence <= 100) {
+    //Attendence Marks
+    if (attendence >= 75 && attendence <= 80) {
+      attendenceMarks = 8;
+    } else if (attendence >= 80 && attendence <= 90) {
+      attendenceMarks = 9;
+    } else if (attendence >= 90 && attendence <= 100) {
+      attendenceMarks = 10;
+    }
+
+    Internals = Math.ceil(
+      cat1 / 4 +
+        cat2 / 4 +
+        attendenceMarks +
+        (assignment1 + assignment2) / 6.666
+    );
+
+    console.log(Internals);
+    console.log("Button clicked");
+  } else {
+  }
 });
