87f73dffb27a0b47af361acdc9734e221f38ddc3
Aathish R Viswam <aathishrv@gmail.com>
2024-07-01 14:49:20 +0530

    script init

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const cat1 = document.querySelector(".cat1");
+const cat2 = document.querySelector(".cat2");
+const attendence = document.querySelector(".attendence");
+const assignment1 = document.querySelector(".assignment1");
+const assignment2 = document.querySelector(".assignment2");
+const submit = document.querySelector(".submit-btn");
+
+submit.addEventListener("click", function () {
+  console.log("Button clicked");
+});
