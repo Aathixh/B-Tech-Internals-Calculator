8e3f144dfcba06de6ca71e8acf895e75c43f23b0
Aathish R Viswam <aathishrv@gmail.com>
2024-07-01 23:17:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,37 +11,41 @@ submit.addEventListener("click", function () {
     parseFloat(document.getElementById("Assignment1").value) || 0;
   const assignment2 =
     parseFloat(document.getElementById("Assignment2").value) || 0;
+  const section1 = document.querySelector(".form");
+  const section2 = document.querySelector(".internals-result");
+  const internalScore = document.getElementById("internals-value");
 
+  section2.classList.remove("hidden", "slide-in");
+  section1.classList.remove("slide-out");
+  internalScore.classList.remove("hidden");
+  document.querySelector(".internal-heading").textContent = "Internals Score";
   if (attendence >= 75 && attendence <= 100) {
     //Attendence Marks
-    if (attendence >= 75 && attendence <= 80) {
+    if (attendence >= 75 && attendence < 80) {
       attendenceMarks = 8;
-    } else if (attendence >= 80 && attendence <= 90) {
+    } else if (attendence >= 80 && attendence < 90) {
       attendenceMarks = 9;
     } else if (attendence >= 90 && attendence <= 100) {
       attendenceMarks = 10;
     }
 
-    Internals = Math.ceil(
-      cat1 / 4 +
-        cat2 / 4 +
-        attendenceMarks +
-        (assignment1 + assignment2) / 6.666
-    );
+    Internals =
+      Math.round(cat1 / 4 + cat2 / 4 + (assignment1 + assignment2) / 6.666) +
+      attendenceMarks;
 
-    document.querySelector(".internals-result").classList.remove("hidden");
-    document.getElementById("internals-value").textContent = Internals;
-    document.querySelector(".internals-result").classList.add("slide-in");
-    document.querySelector(".form").classList.add("slide-out");
+    section2.classList.remove("hidden");
+    internalScore.textContent = Internals;
+    section2.classList.add("slide-in");
+    section1.classList.add("slide-out");
 
     console.log(Internals);
     console.log("Button clicked");
   } else {
-    document.getElementById("internals-value").classList.add("hidden");
-    document.querySelector(".internals-result").classList.remove("hidden");
+    internalScore.classList.add("hidden");
+    section2.classList.remove("hidden");
     document.querySelector(".internal-heading").textContent =
       "You need Minimum of 75% Attendence";
-    document.querySelector(".internals-result").classList.add("slide-in");
-    document.querySelector(".add-card").classList.add("slide-out");
+    section2.classList.add("slide-in");
+    section1.classList.add("slide-out");
   }
 });
